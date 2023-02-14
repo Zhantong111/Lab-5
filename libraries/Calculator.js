54d7abaa1b009e3afbb92eb9d0ffb24fc54b79b7
Zhan Tong <zhantong94@gmail.com>
2023-02-14 17:35:09 +0800

    Exercise 5 Part 2

--- libraries/Calculator.js
+++ libraries/Calculator.js
@@ -1,6 +1,10 @@
 class Calculator {
   constructor() {
-    this.id = Date.now();
+    this.id = this.#getRandomInt(10000);
+  }
+
+  #getRandomInt(max) {
+    return Math.floor(Math.random() * max);
   }
 
   #log = (value) => {
