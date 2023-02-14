c886303a602b26664e5a5f9ec78cf26f86f15b23
Zhan Tong <zhantong94@gmail.com>
2023-02-14 11:11:28 +0800

    Add server code

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/profile", (req, res) => {
+  res.send("You are at your profile page");
+});
+
+app.listen(port, () => {
+  console.log(`Web server running at port ${port}`);
+});
