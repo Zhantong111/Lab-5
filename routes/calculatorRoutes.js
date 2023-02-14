6a5455b9e7353d4b2bafe23324e5768d4fa8246d
Zhan Tong <zhantong94@gmail.com>
2023-02-14 15:56:29 +0800

    Add calculatorRoutes

--- routes/calculatorRoutes.js
+++ routes/calculatorRoutes.js
@@ -1,21 +1,13 @@
 const express = require("express");
-const calculatorController = require("../controllers/calculatorController");
 const router = express.Router();
 
 router.get("/add", (req, res) => {
-  calculatorController.addNumbers(req, res);
-});
-
-router.get("/subtract", (req, res) => {
-  calculatorController.subtractNumbers(req, res);
-});
-
-router.get("/multiply", (req, res) => {
-  calculatorController.multiplyNumbers(req, res);
-});
-
-router.get("/divide", (req, res) => {
-  calculatorController.divideNumbers(req, res);
+  let number1 = parseInt(req.query.num1);
+  let number2 = parseInt(req.query.num2);
+  let sum = number1 + number2;
+  console.log(sum);
+  res.status(200);
+  res.json({ result: sum });
 });
 
 module.exports = router;
