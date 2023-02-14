077a57025dd8ef9be674ec09c81cd195d7af04e4
Zhan Tong <zhantong94@gmail.com>
2023-02-14 18:00:00 +0800

    Exercise 7

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 3000;
 
 app.use("/", express.static("public"));
 
+const swaggerUi = require("swagger-ui-express");
+swaggerDocument = require("./swagger.json");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 const userRoutes = require("./routes/userRoutes");
 app.use("/users", userRoutes);
 
