d7989ef7aff8c3884f9538b033dda2b0c910cd1b
Zhan Tong <zhantong94@gmail.com>
2023-02-14 11:24:29 +0800

    Exercise 1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const port1 = 8000;
+const port2 = 80;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -12,4 +14,12 @@ app.get("/profile", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Web server running at port ${port}`);
+});
+
+app.listen(port1, () => {
+  console.log(`Web server running at port ${port1}`);
+});
+
+app.listen(port2, () => {
+  console.log(`Web server running at port ${port2}`);
 });
